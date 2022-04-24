dc3e09f7d5e3afb4148a0d87cb09fb18ce9a6014
anurkhalizaah <adenurkhalizaaa@gmail.com>
2022-04-24 20:40:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ekantin/responsi/loginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ekantin.responsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ekantin.R
+
+class loginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
